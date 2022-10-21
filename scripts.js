2591644aec7f4f62e8f222e7bbcddad840e3d942
DavidLynch26 <lynchda78@gmail.com>
2022-10-21 11:21:13 -0500

    Finished js button, Added grid to body of css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+'use strict';
+
+window.onload = loadPage;
+
+function loadPage(){
+    let submit = document.querySelectorAll(".btn")[1];
+
+    console.log(submit);
+        
+    submit.addEventListener('click', clicker);
+}
+
+function clicker(event){
+    event.preventDefault();
+    var emailAddress = document.getElementById("email").value;
+
+    console.log(emailAddress);
+
+    document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +emailAddress+ " has been added to our mailing list";
+    console.log(document.getElementsByClassName("message")[0].innerHTML);
+}
